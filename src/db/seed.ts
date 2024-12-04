ace374681355d470e354d68117450e7441e4d92a
pheggeseth <pheggeseth@gmail.com>
2024-12-04 10:59:55 -0600

    simplify seed script

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,12 +2,9 @@ import { db } from './db';
 import { tasksTable } from './schema';
 
 export async function seed() {
-	await db.delete(tasksTable);
 	await db.insert(tasksTable).values({
 		title: 'world domination',
 	});
-	const tasks = await db.select().from(tasksTable);
-	console.log({ tasks });
 }
 
 seed();
