20a6b57c72d71bbf529b1719b9f5532ada150a13
pheggeseth <pheggeseth@gmail.com>
2024-12-04 21:20:38 -0600

    track dates in ms

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,10 +3,10 @@ import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const tasksTable = sqliteTable('tasks_table', {
 	id: int().primaryKey({ autoIncrement: true }),
 	title: text().notNull(),
-	creationDate: int({ mode: 'timestamp' })
+	creationDate: int({ mode: 'timestamp_ms' })
 		.notNull()
 		.$defaultFn(() => new Date()),
-	completionDate: int({ mode: 'timestamp' }),
+	completionDate: int({ mode: 'timestamp_ms' }),
 });
 
 export type Task = typeof tasksTable.$inferSelect;
